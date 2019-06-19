e9364479008a3db0df25ef3db590632e06d03715
ahasan997 <ahasan997@gmail.com>
2019-06-19 15:38:29 +0600

    added ProductDTO

--- src/main/java/men/ahasan/spring_mysql_thymeleaf_jpa_crud/controller/AppController.java
+++ src/main/java/men/ahasan/spring_mysql_thymeleaf_jpa_crud/controller/AppController.java
@@ -1,13 +1,17 @@
 package men.ahasan.spring_mysql_thymeleaf_jpa_crud.controller;
 
 import men.ahasan.spring_mysql_thymeleaf_jpa_crud.model.Product;
+import men.ahasan.spring_mysql_thymeleaf_jpa_crud.model.ProductDTO;
 import men.ahasan.spring_mysql_thymeleaf_jpa_crud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,7 +26,6 @@ public class AppController {
         return "index";
     }
 
-
     @GetMapping("/viewproduct")
     public String viewAllProduct(Model model) {
         List<Product> listProducts = service.listAll();
@@ -33,15 +36,21 @@ public class AppController {
 
     @GetMapping("/new")
     public String showNewProductForm(Model model) {
-        Product product = new Product();
-        model.addAttribute(product);
+        ProductDTO productDTO = new ProductDTO();
+        model.addAttribute(productDTO);
 
         return "new_product";
     }
 
     @PostMapping("/save")
-    public String saveProduct(@ModelAttribute("product") Product product) {
-        service.save(product);
+    public String saveProduct(@Valid ProductDTO productDTO, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {
+            System.out.println("has errors");
+            return "new_product";
+        }
+
+        service.save(productDTO.getProduct());
 
         return "redirect:/viewproduct";
     }
